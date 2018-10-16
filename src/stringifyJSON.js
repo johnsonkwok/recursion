a2baf2a9572c9498b3425f9f7e13de8e8540cec1
Johnson Kwok <johnsonkwok1@gmail.com>
2018-10-16 13:22:56 -0700

    Implement the stringifyJSON function

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,32 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if (obj === null || Number.isNaN(obj) || obj === Infinity || obj === -Infinity) {
+    return 'null';
+  } else if (typeof obj === 'boolean' || typeof obj === 'number') {
+    return `${obj}`;
+  } else if (typeof obj === 'string') {
+    return `"${obj}"`;
+  } else if (Array.isArray(obj)) {
+    return '[' + obj.reduce(function(acc, cv) {
+      if (typeof cv === 'undefined' || typeof cv === 'function') {
+        return [...acc, 'null'];
+      } else {
+        return [...acc, stringifyJSON(cv)];
+      }
+    }, []).join(',') + ']';
+  } else if (typeof obj === 'object') {
+    const objKeys = Object.keys(obj);
+    let stringifiedObj = '';
+    for (let key in obj) {
+      if (obj[key] === undefined || typeof obj[key] === 'function') {
+        stringifiedObj += '';
+      } else if (key === objKeys[objKeys.length - 1]) {
+        stringifiedObj += `"${key}":` + stringifyJSON(obj[key]);
+      } else {
+        stringifiedObj += `"${key}":` + stringifyJSON(obj[key]) + ',';
+      }
+    }
+    return '{' + stringifiedObj + '}';
+  }
 };
