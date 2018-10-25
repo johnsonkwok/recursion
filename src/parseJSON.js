96f63a03cead2d02454bb50d533628117ee83f72
Johnson Kwok <johnsonkwok1@gmail.com>
2018-10-25 00:47:55 -0700

    Implement most of parseJSON function (covers most basic cases)
    * Refactor parseJSON function in order to cover more cases
    * All tests do not pass, some edge cases not covered

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,100 +3,195 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
-  // identify item to be parsed
+  // start with first character of json string
+    // create variables for current char and index of current char
+  // invoke parseVal function, which identifies item to be parsed
     // use appropriate helper function to parse that item
+    // make recursive calls to parseVal function as needed for objects and arrays and nested values
+    // throw error if invalid JSON inputted
 
-  const firstChar = json[0];
-  const lastChar = json[json.length - 1];
-  let currCharIdx = 0;
-  const parsedArr = [];
-  const parsedObj = {};
+  let currChIdx = 0;
+  let currCh = json[currChIdx];
 
-  if (firstChar === '"' && lastChar === '"') {
-    return parseStr(json);
-  } else if (json === 'true' || json === 'false' || json === 'null') {
-    return parseBool(json);
-  } else if (!Number.isNaN(Number(json)) && json !== '') {
-    return parseNum(json);
-  } else if (firstChar === '[' && lastChar === ']') {
-    return parseArr(json);
-  } else if (firstChar === '{' && lastChar === '}') {
-    return parseObj(json);
-  } else {
-    throw new SyntaxError('Invalid stringified JSON');
+  return parseVal();
+
+  function parseVal() {
+    if (currCh === '"') {
+      return parseStr();
+    } else if (currCh === 't' || currCh === 'f' || currCh === 'n') {
+      return parseBool();
+    } else if (currCh === '[') {
+      return parseArr();
+    } else if (currCh === '{') {
+      return parseObj();
+    } else if (currCh === '-' || (currCh && currCh >= 0 && currCh <= 9)) {
+      return parseNum();
+    } else {
+      throwErr();
+    }
   }
   
+  function throwErr() {
+    throw new SyntaxError('Invalid stringified JSON');
+  }
 
-  function setNextCharIdx() {
-    currCharIdx++;
-    if (firstChar !== '"' && json[currCharIdx] === ' ') {
-      setNextCharIdx();
+  function setNextCh() {
+    // this function is used to keep track of the current char    
+      // also returns the current char, which may be useful for moving to the next item of an array or object
+    currChIdx++;
+    currCh = json[currChIdx];
+    return currCh;
+  }
+
+  function parseStr() {
+    // create empty holder string variable
+      // add chars to holder string variable until a end quote is reached
+    // return string
+    let str = '';
+    setNextCh();
+
+    const escapes = {
+      'b': '\b',
+      'n': '\n',
+      't': '\t',
+      'r': '\r',
+      'f': '\f',
+      '\"': '\"',
+      '\\': '\\'
+    };
+    
+    while (currCh) {
+      if (currCh === '"') {
+        setNextCh();
+        return str;
+      }
+
+      if (currCh === '\\') {
+        setNextCh();
+        if (escapes.hasOwnProperty(currCh)) {
+          str += escapes[currCh];
+        } else {
+          str += currCh;
+        }
+      } else {
+        str += currCh;
+      }
+
+      setNextCh();
+    }
+
+    throwErr();
+  }
+
+  function parseNum() {
+    // create empty holder string variable for the number
+      // add negative sign or digits as needed
+      // add decimal point if present
+        // add additional digits after decimal point
+    // convert string to a number and return number
+    let num = '';
+    
+    function getDigits() {
+      while (currCh && currCh >= 0 && currCh <= 9) {
+        num += currCh;
+        setNextCh();
+      }
+    }
+
+    if (currCh === '-') {
+      num += currCh;
+      setNextCh();
+    }
+
+    getDigits();
+
+    if (currCh === '.') {
+      num += currCh;
+      setNextCh();
+      getDigits();
+    }
+
+    if (!Number.isNaN(Number(num))) {
+      return Number(num);
+    } else {
+      throwErr();
     }
   }
 
-  function parseStr(jsonStr) {
-    return jsonStr.slice(1, -1);
-  }
+  function parseBool() {
+    // create empty holder variable for boolean/null
+    // get the next 4 or 5 chars as needed
+      // if boolean string equals 'true' or 'null'
+        // return true or null, respectively
+      // if boolean string equals 'false'
+        // return false
+    let bool = '';
+    for (let i = 0; i < 4; i++) {
+      bool += currCh;
+      setNextCh();
+    }
 
-  function parseNum(jsonNum) {
-    return Number(jsonNum);
-  }
-
-  function parseBool(jsonBool) {
-    if (jsonBool === 'true') {
+    if (bool === 'true') {
       return true;
-    } else if (jsonBool === 'false') {
-      return false;
-    } else if (jsonBool === 'null') {
+    } else if (bool === 'null') {
       return null;
+    } else if (bool === 'fals') {
+      bool += currCh;
+      setNextCh();
+      return false;
+    } else {
+      throwErr();
     }
   } 
-
-  function parseObj(jsonObj) {
-
-  }
-
-  function parseMember(jsonMemb) {
-
-  }
-
-  function parsePair(jsonPair) {
-
-  }
 
   function parseArr(jsonArr) {
-    if (jsonArr === '[]') {
-      return [];
-    } else {
-      setNextCharIdx();
-      return parseElem(jsonArr.slice(currCharIdx, -1));
+    // create holder array variable
+    // if next char is end bracket
+      // return empty array
+    // else use do...while loop to push value(s) into holder array
+      // do...while loop is used because we know there is at least one value to be added to the holder array (not an empty array)
+      // if the char after the value is a comma
+        // set the next char in the json and continue loop
+    const arr = [];
+    if (setNextCh() === ']') {
+      return arr;
     }
+
+    do {
+      arr.push(parseVal());
+      if (currCh === ']') {
+        setNextCh();
+        return arr;
+      }
+    } while (currCh === ',' && setNextCh());
+
+    throwErr();
   }
 
-  function parseElem(jsonElem) {
-    if (!jsonElem.includes(',')) {
-      parsedArr.push(parseJSON(jsonElem));
-      return parsedArr;
-    } else {
-      let commaIdx = (jsonElem.indexOf(','));
-      if (jsonElem[0] === '"') {
-        commaIdx = jsonElem.search(/",/) + 1;
-      } else if (jsonElem[0] === '[') {
-        if (jsonElem[jsonElem.length - 1] === ']') {
-          commaIdx = jsonElem.length;
-        } else {
-          commaIdx = jsonElem.search(/],/) + 1;
-        }
-      } else if (jsonElem[0] === '{') {
-        commaIdx = jsonElem.search(/},/) + 1;
-      }
-      // console.log(jsonElem, commaIdx);
-      parsedArr.push(parseJSON(jsonElem.slice(0, commaIdx)));
-      // currCharIdx += commaIdx;
-      // setNextCharIdx();
-      let remainingItems = jsonElem.slice(commaIdx + 1);
-      return remainingItems ? parseElem(remainingItems) : parsedArr;
+  function parseObj(jsonObj) {
+    // create holder object variable
+    // if next char is end brace
+      // return empty object
+    // else use do...while loop to add key:value pairs into holder object
+      // do...while loop is used because we know there is at least one key:value pair to be added to the holder object (not an empty object)
+      // if the char after the value is a comma
+        // set the next char in the json and continue loop
+    const obj = {};
+    if (setNextCh() === '}') {
+      return obj;
     }
-  } 
+
+    do {
+      const key = parseStr();
+      setNextCh();
+      obj[key] = parseVal();
+      if (currCh === '}') {
+        setNextCh();
+        return obj;
+      }
+    } while (currCh === ',' && setNextCh());
+
+    throwErr();
+  }
 
 };
